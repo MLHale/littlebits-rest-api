d3b045255979ba1d5ad4175fdb695deb2b65a9cc
Matt Hale <mlhale@unomaha.edu>
2017-07-21 14:32:15 -0500

    fix one more import statement

--- api/controllers.py
+++ api/controllers.py
@@ -8,7 +8,6 @@ from rest_framework.views import APIView
 from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
 from rest_framework import status
-from api.serializers import *
 #from django.shortcuts import render_to_response
 from django.template import RequestContext
 from django_filters.rest_framework import DjangoFilterBackend
